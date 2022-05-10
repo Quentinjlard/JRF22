cb8591cb94a1c153bd96e538718357e7db61b02f
Tehcam <tehcam42@gmail.com>
2022-05-10 20:21:24 +0200

    configuration

--- quasar.config.js
+++ quasar.config.js
@@ -58,7 +58,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-webpack/quasar-config-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
@@ -106,7 +106,7 @@ module.exports = configure(function (ctx) {
       // directives: [],
 
       // Quasar plugins
-      plugins: []
+      plugins: [ 'Notify' ]
     },
 
     // animations: 'all', // --- includes all animations
