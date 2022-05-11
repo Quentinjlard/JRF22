f8fc1c83c7c202f470f46b8743659c1ca55b6fcb
Tehcam <tehcam42@gmail.com>
2022-05-11 22:34:42 +0200

    footer et responsive

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,6 +2,9 @@
   <q-layout view="lHh Lpr lFf">
     <q-page-container>
       <router-view />
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-btn fab-mini icon="expand_less" color="accent" to="#" />
+      </q-page-sticky>
     </q-page-container>
   </q-layout>
 </template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -3,22 +3,26 @@
     <div class="full-width m-content_container">
       <q-tabs
         v-model="slide"
-        class="text-grey bg-primary"
+        class="text-info bg-primary"
         active-color="secondary"
         indicator-color="secondary"
         align="justify"
         narrow-indicator
       >
-        <q-tab name="index" label="Accueil" />
-        <q-tab name="program" label="Programme" />
+        <q-tab name="index" label="Accueil" v-if="Screen.width > 500" />
+        <q-tab name="index" icon="home" v-else />
+        <q-tab name="program" label="Programme" v-if="Screen.width > 500" />
+        <q-tab name="program" icon="insert_chart" v-else />
         <q-img
           src="icons/favicon-32x32.png"
           height="32px"
           width="32px"
           class="q-mx-md"
         />
-        <q-tab name="pay" label="S'inscrire" />
-        <q-tab name="pictures" label="Photos" />
+        <q-tab name="pay" label="S'inscrire" v-if="Screen.width > 500" />
+        <q-tab name="pay" icon="credit_card" v-else />
+        <q-tab name="pictures" label="Photos" v-if="Screen.width > 500" />
+        <q-tab name="pictures" icon="photo_library" v-else />
       </q-tabs>
 
       <q-separator />
@@ -38,11 +42,28 @@
         </q-tab-panel>
       </q-tab-panels>
     </div>
+    <div class="full-width bg-primary text-info q-pa-lg m-mentions_container">
+      <div>
+        <div class="q-px-sm m-mentions_section">
+          <div class="text-h6">Mentions légales</div>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+        </div>
+        <div class="q-px-sm m-mentions_section">
+          <div class="text-h6">Liens</div>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+        </div>
+        <div class="q-px-sm m-mentions_section">
+          <div class="text-h6">Contacts</div>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+import { Screen } from 'quasar'
 import MIndex from 'src/components/MIndex.vue'
 import MPay from 'src/components/MPay.vue'
 import MPictures from 'src/components/MPictures.vue'
@@ -50,3 +71,25 @@ import MProgram from 'src/components/MProgram.vue'
 
 const slide = ref('index')
 </script>
+
+<style>
+.m-mentions_container > div {
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.m-mentions_section {
+  margin: 0 0 0 1.5rem;
+}
+
+@media screen and (max-width: 500px) {
+  .m-mentions_container > div {
+    flex-direction: column;
+  }
+
+  .m-mentions_section {
+    margin: 1.5rem 0 0 0;
+  }
+}
+</style>
