d1600e30d819a41423c565d28101fc641a15a595
Tehcam <tehcam42@gmail.com>
2022-05-11 22:42:36 +0200

    améliore scroll top

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,8 +3,18 @@
     <q-page-container>
       <router-view />
       <q-page-sticky position="bottom-right" :offset="[18, 18]">
-        <q-btn fab-mini icon="expand_less" color="accent" to="#" />
+        <q-btn fab-mini icon="expand_less" color="accent" @click="scrollTop" />
       </q-page-sticky>
     </q-page-container>
   </q-layout>
 </template>
+
+<script lang="ts" setup>
+import { scroll } from 'quasar'
+
+const { setVerticalScrollPosition } = scroll
+
+function scrollTop(): void {
+  setVerticalScrollPosition(window, 0, 250)
+}
+</script>
