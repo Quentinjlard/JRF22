baf8fe755f8a19fd9fd2fb2f08d90abd97ac45db
Tehcam <tehcam42@gmail.com>
2022-05-11 21:17:54 +0200

    squelette de la page

---
+++ src/components/MPictures.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perspiciatis unde accusantium quibusdam odit ea tempore molestias atque, expedita, reprehenderit nesciunt nisi aliquid nihil aut corporis. Nostrum dolorem repudiandae iusto.
+    </div>
+</template>
+
+<script lang="ts" setup>
+// import { ref } from 'vue'
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,52 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <div>Hello World !</div>
+  <q-page class="fit absolute-center">
+    <div class="full-width m-content_container">
+      <q-tabs
+        v-model="slide"
+        class="text-grey bg-primary"
+        active-color="secondary"
+        indicator-color="secondary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="index" label="Accueil" />
+        <q-tab name="program" label="Programme" />
+        <q-img
+          src="icons/favicon-32x32.png"
+          height="32px"
+          width="32px"
+          class="q-mx-md"
+        />
+        <q-tab name="pay" label="S'inscrire" />
+        <q-tab name="pictures" label="Photos" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="slide" animated :transition-duration="300" swipeable infinite style="min-height: 100vh">
+        <q-tab-panel name="index" class="m-content_slide">
+          <m-index />
+        </q-tab-panel>
+        <q-tab-panel name="program" class="m-content_slide">
+          <m-pay />
+        </q-tab-panel>
+        <q-tab-panel name="pay" class="m-content_slide">
+          <m-program />
+        </q-tab-panel>
+        <q-tab-panel name="pictures" class="m-content_slide">
+          <m-pictures />
+        </q-tab-panel>
+      </q-tab-panels>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts" setup>
-// import { ref } from 'vue'
+import { ref } from 'vue'
+import MIndex from 'src/components/MIndex.vue'
+import MPay from 'src/components/MPay.vue'
+import MPictures from 'src/components/MPictures.vue'
+import MProgram from 'src/components/MProgram.vue'
+
+const slide = ref('index')
 </script>
