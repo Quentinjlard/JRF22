8c1301eba312596dc7cd9444537416cb1127e2c5
Tehcam <tehcam42@gmail.com>
2022-05-28 19:29:54 +0200

    debug import

--- src/boot/google-maps.js
+++ src/boot/google-maps.js
@@ -1,6 +1,6 @@
 import { boot } from 'quasar/wrappers'
 import VueGoogleMaps from '@fawmi/vue-google-maps'
-import { googleMapsApiKey } from 'variables.env'
+import { googleMapsApiKey } from '../../variables'
 
 export default boot(({ app }) => {
     app.use(VueGoogleMaps, {
